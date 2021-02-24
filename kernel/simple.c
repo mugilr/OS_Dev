7140c24e6da85d047a4dc749bbd36d0acb5a4b7b
Mugilraj Dhavachelvan <dmugil2000@gmail.com>
2021-02-24 21:14:35 +0530

    added readme and C smple program

---
+++ kernel/simple.c
@@ -0,0 +1,24 @@
+int fun1(){
+	return 0x12;
+}
+int fun2(){
+	int i=1234;
+	return 0x123f;
+}
+int fun3(){
+	int i=34;
+	int j=i;
+	return 0x123f;
+}
+int add(int a, int b){
+	return a+b;
+}
+int caller(){
+	int res= add(2,3);
+}
+int addp(int *a, int *b){
+	return *a + *b;
+}
+int callerp(){
+	int res= add(2,3);
+}
